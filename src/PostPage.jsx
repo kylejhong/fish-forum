8aafaabb0292f64cc5586d432e889c035d1709c3
Kyle Jhong <29828796+kylejhong@users.noreply.github.com>
2025-08-03 13:28:12 -0700

    Working on PostPage

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -5,6 +5,8 @@ import { supabase } from "./client";
 const PostPage = () => {
     const { id } = useParams();
     const [post, setPost] = useState(null);
+    const [diffHours, setDiffHours] = useState(null);
+    const [diffDays, setDiffDays] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -21,10 +23,45 @@ const PostPage = () => {
 
         fetchPost().catch(console.error)
     }, [id]);
+
+    useEffect(() => {
+        if (post) {
+            const timestamp = new Date(post.created_at);
+            const now = new Date();
+
+            const diffMs = now - timestamp;
+            setDiffHours(Math.round(diffMs / (1000 * 60 * 60)));
+            setDiffDays(Math.round(diffHours / (24)));
+        }
+    }, [post, diffHours]);
+
+    const imageExists = (url) => {
+        var image = new Image();
+        image.src = url;
+        if (image.width == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
     
     return (
         <div>
-            {post ? (<p>{JSON.stringify(post)}</p>) : (null)}
+            {post ? (
+                <>
+                    <p>Created {diffDays > 0 ? (`${diffDays} day${diffDays == 1 ? ("") : ("s")} ago`) : (`${diffHours} hour${diffHours == 1 ? ("") : ("s")} ago`)}</p>
+                    <p>{JSON.stringify(post)}</p>
+                    <h1>{post.title}</h1>
+                    <p>{post.content}</p>
+                    <p>{post.likes}</p>
+                    <p>{post.comments}</p>
+                    {imageExists(post.image) ? (
+                        <img src={post.image}/>
+                    ) : (
+                        null
+                    )}
+                </>
+            ) : (null)}
         </div>
     )
 }
